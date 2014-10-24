71e2f4475dd7a96fd93933afff795c68deb79d37
Nils Jonsson <nils@alumni.rice.edu>
2014-10-24 13:00:03 -0500

    Avoid platform-specific spec failures due to fileglob ordering

--- spec/autoloaded/constant_spec.rb
+++ spec/autoloaded/constant_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Autoloaded::Constant do
   describe 'for' do
     let(:directory) { 'spec/fixtures/filenames' }
 
-    {AFilename:  %w(a_filename  a-file-name a-filename a_file_name             AFilename),
-     A_FILENAME: %w(a_filename  a-file-name a-filename a_file_name             AFilename),
-     AFileName:  %w(a_file_name a-file-name a-filename             a_filename  AFilename),
-     AFILEName:  %w(a-file-name             a-filename a_file_name a_filename  AFilename)}.each do |constant_name,
-                                                                                                    filenames|
+    {AFilename:  %w(a_filename  a-file-name a-filename a_file_name             afile_name afile-name AFilename),
+     A_FILENAME: %w(a_filename  a-file-name a-filename a_file_name             afile_name afile-name AFilename),
+     AFileName:  %w(a_file_name a-file-name a-filename             a_filename  afile_name afile-name AFilename),
+     AFILEName:  %w(afile_name  a-file-name a-filename a_file_name a_filename             afile-name AFilename)}.each do |constant_name,
+                                                                                                                          filenames|
       describe constant_name.inspect do
         let(:constant) { constant_class.new constant_name }
 
@@ -21,11 +21,25 @@ RSpec.describe Autoloaded::Constant do
         }
 
         describe '#each_matching_filename_in' do
-          specify {
-            expect { |block|
-              constant.each_matching_filename_in(directory, &block)
-            }.to yield_successive_args(*full_filenames)
+          let(:yielded_args) {
+            result = []
+            constant.each_matching_filename_in directory do |filename|
+              result << filename
+            end
+            result
           }
+
+          describe 'first yielded argument' do
+            subject(:first_yielded_argument) { yielded_args.first }
+
+            it { is_expected.to eq(full_filenames.first) }
+          end
+
+          describe 'subsequent yielded arguments' do
+            subject(:subsequent_yielded_arguments) { yielded_args[1..-1] }
+
+            it { is_expected.to match_array(full_filenames[1..-1]) }
+          end
         end
       end
     end
