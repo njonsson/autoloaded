da1e287d6e38121af1bab6ced201e2a0fa3fb5cb
Nils Jonsson <nils@alumni.rice.edu>
2014-10-27 15:39:40 -0500

    Use process forking instead of squirrelly shell integration

--- spec/matchers.rb
+++ spec/matchers.rb
@@ -8,36 +8,27 @@ RSpec::Matchers.define :autoload_a_constant_named do |constant_name|
     constant_tokens = constant_name.split('::')
     constant_up_till_last = constant_tokens[0...-1].join('::')
     constant_last = constant_tokens.last
-    assertion = <<-end_assertion.gsub(/^      /, '')
-      begin;
-        #{constant_name};
-      rescue;
-      else;
-        raise('Constant #{constant_name} is already defined');
-      end;
-
-      load #{source_file.inspect};
-
-      puts(begin;
-             #{constant_name};
-           rescue;
-             'false';
-           else;
-             #{constant_up_till_last}.autoload?(:#{constant_last}) ?
-               'true'                                              :
-               'false';
-           end)
-    end_assertion
-    quoted_assertion = assertion.chomp.inspect.gsub('\n', "\n")
-    Open3.popen3 "bundle exec ruby -e #{quoted_assertion}" do |stdin,
-                                                               stdout,
-                                                               stderr|
-      if (stderr_output = stderr.read.chomp).empty?
-        eval stdout.read.chomp
+    pid = fork do
+      begin
+        eval constant_name
+      rescue
       else
-        raise stderr_output
+        raise("Constant #{constant_name} is already defined")
+      end
+
+      load source_file
+
+      begin
+        eval constant_name
+      rescue
+        exit 1
+      end
+      unless eval(constant_up_till_last).autoload?(constant_last.to_sym)
+        exit 1
       end
     end
+    Process.wait pid
+    $?.success?
   end
 end
 
@@ -52,30 +43,36 @@ RSpec::Matchers.define :define_only_constants_named do |*constant_names|
       raise "Missing .in_a_namespace_named(:Namespace) clause"
     end
 
-    assertion = <<-end_assertion.gsub(/^      /, '')
-      load #{source_file.inspect};
+    reader, writer = IO.pipe
+    pid = fork do
+      reader.close
 
-      puts(begin;
-             #{constant_names.inspect}.each do |constant_name|
-               #{namespace_name}.const_get constant_name;
-             end;
-             #{namespace_name}.constants.inspect;
-           rescue;
-             '[]';
-           end)
-    end_assertion
-    quoted_assertion = assertion.chomp.inspect.gsub('\n', "\n")
-    Open3.popen3 "bundle exec ruby -e #{quoted_assertion}" do |stdin,
-                                                               stdout,
-                                                               stderr|
-      if (stderr_output = stderr.read.chomp).empty?
-        defined_constants = Array(eval(stdout.read.chomp)).sort.collect(&:to_sym)
-        @expected_constants = constant_names.sort.collect(&:to_sym)
-        @extraneous_defined_constants = defined_constants - expected_constants
-        extraneous_defined_constants.empty?
-      else
-        raise stderr_output
+      load source_file
+
+      begin
+        constant_names.each do |constant_name|
+          eval(namespace_name).const_get constant_name
+        end
+        eval(namespace_name).constants.each do |constant_name|
+          writer.puts constant_name.inspect
+        end
+      rescue
       end
+    end
+    Process.wait pid
+    writer.close
+    if $?.success?
+      defined_constants = []
+      reader.each_line do |line|
+        binding.pry
+        defined_constants << eval(line.chomp)
+      end
+      defined_constants = defined_constants.sort.collect(&:to_sym)
+      @expected_constants = constant_names.sort.collect(&:to_sym)
+      @extraneous_defined_constants = defined_constants - expected_constants
+      extraneous_defined_constants.empty?
+    else
+      false
     end
   end
 
