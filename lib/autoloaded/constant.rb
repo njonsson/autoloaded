812313fd1f2c41461425d13d7847b0e32d2bd15c
Nils Jonsson <nils@alumni.rice.edu>
2014-10-27 17:17:42 -0500

    Refactor Constant#each_matching_filename_in for complexity

--- lib/autoloaded/constant.rb
+++ lib/autoloaded/constant.rb
@@ -10,26 +10,16 @@ class Autoloaded::Constant
   attr_reader :name
 
   def initialize(name)
-    @name = name
+    @name = name.freeze
   end
 
-  def each_matching_filename_in(directory)
+  def each_matching_filename_in(directory, &block)
     filenames = ::Set.new
 
-    ::Dir.chdir directory do
-      filename = [name.to_s.to_source_filename, extension].join
-      if ::File.file?(filename) && filenames.add?(filename)
-        yield ::File.join(directory, filename)
-      end
-
-      name_signature = signature(name)
-      ::Dir.glob(['*', extension].join, ::File::FNM_CASEFOLD) do |f|
-        f_signature = signature(::File.basename(f, extension))
-        if (f_signature == name_signature) && filenames.add?(f)
-          yield ::File.join(directory, f)
-        end
-      end
-    end
+    yield_qualified_name_source_filename_in(directory,
+                                            unless_in: filenames,
+                                            &block)
+    yield_all_qualified_filenames_in(directory, unless_in: filenames, &block)
   end
 
 private
@@ -38,8 +28,37 @@ private
     '.rb'
   end
 
+  def name_signature
+    @name_signature ||= signature(name)
+  end
+
+  def name_source_filename
+    @name_source_filename ||= [name.to_s.to_source_filename, extension].join
+  end
+
+  def qualified_glob_in(directory)
+    ::File.join(directory, ['*', extension].join)
+  end
+
   def signature(string)
     string.to_s.gsub(/[^a-z0-9]/i, '').downcase
+  end
+
+  def yield_all_qualified_filenames_in(directory, unless_in:, &block)
+    ::Dir.glob(qualified_glob_in(directory), ::File::FNM_CASEFOLD) do |filename|
+      filename_signature = signature(::File.basename(filename, extension))
+      if (filename_signature == name_signature) &&
+         unless_in.add?(::File.basename(filename))
+        block.call filename
+      end
+    end
+  end
+
+  def yield_qualified_name_source_filename_in(directory, unless_in:, &block)
+    qualified = ::File.join(directory, name_source_filename)
+    if ::File.file?(qualified) && unless_in.add?(name_source_filename)
+      block.call qualified
+    end
   end
 
 end
